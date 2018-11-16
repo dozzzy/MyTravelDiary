143a95625e7c55d2dde03f1a29452ded73521239
Yiwen <yiweng@bu.edu>
2018-11-16 15:31:59 -0500

    Create PlaceDAO.java

---
+++ app/src/main/java/com/example/a15862/mytraveldiary/DAO/PlaceDAO.java
@@ -0,0 +1,18 @@
+package com.example.a15862.mytraveldiary.DAO;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class PlaceDAO {
+    private FirebaseFirestore db;
+
+
+    public PlaceDAO(){
+        db = FirebaseFirestore.getInstance();
+    }
+
+    public void insertPlace(){
+
+    }
+
+
+}
