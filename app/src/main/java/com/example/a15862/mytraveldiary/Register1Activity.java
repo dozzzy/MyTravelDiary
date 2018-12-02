3326ac221e5a60f90eec58f745dcea513ec141b7
HUI HAO <haohuily@bu.edu>
2018-12-02 14:42:22 -0500

    "Register pages"

---
+++ app/src/main/java/com/example/a15862/mytraveldiary/Register1Activity.java
@@ -0,0 +1,13 @@
+package com.example.a15862.mytraveldiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register1);
+    }
+}
