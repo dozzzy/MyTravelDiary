fca1b97cecca8b4892f35c2aba70729d395da85c
HUI HAO <haohuily@bu.edu>
2018-12-02 15:44:42 -0500

    Modify Dialog UI

--- app/src/main/java/com/example/a15862/mytraveldiary/ClickNotExistActivity.java
+++ app/src/main/java/com/example/a15862/mytraveldiary/ClickNotExistActivity.java
@@ -1,12 +1,13 @@
 package com.example.a15862.mytraveldiary;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class ClickNotExistActivity extends AppCompatActivity {
+public class ClickNotExistActivity extends Activity {
     private Button btnJump;
 
     @Override
