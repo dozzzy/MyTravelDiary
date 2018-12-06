a49d51ca40b961d25d91f7fbbd5e35c344a9eb43
jingdian <jingdian@bu.edu>
2018-12-05 20:59:35 -0500

    Merge branch 'master' of https://github.com/dozzzy/MyTravelDiary
    # Conflicts:
    #	app/src/main/java/com/example/a15862/mytraveldiary/ClickNotExistActivity.java

--- app/src/main/java/com/example/a15862/mytraveldiary/ViewAllFriendsActivity.java
+++ app/src/main/java/com/example/a15862/mytraveldiary/ViewAllFriendsActivity.java
@@ -3,11 +3,17 @@ package com.example.a15862.mytraveldiary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ViewAllFriendsActivity extends AppCompatActivity {
+import com.example.a15862.mytraveldiary.Entity.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewAllFriendsActivity extends AppCompatActivity {
+    List<User> friends = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all_friends);
+
     }
 }
